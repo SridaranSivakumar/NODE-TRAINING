361d7bf3c2235f0f362f43ab7d792fe42ddc1ff7
SridaranSivakumar <sridaran.sivakumar@cdw.com>
2023-04-09 21:17:44 +0530

    changes

--- Exercise-6/utils/logger.js
+++ Exercise-6/utils/logger.js
@@ -10,4 +10,13 @@ module.exports = createLogger({
             format.align(),
             format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
         )}),
+    transports:
+        new transports.File({
+            level: "warn",
+        filename: 'logs/warn.log',
+        format:format.combine(
+            format.timestamp({format: 'MMM-DD-YYYY HH:mm:ss'}),
+            format.align(),
+            format.printf(info => `${info.level}: ${[info.timestamp]}: ${info.message}`),
+        )}),
 });
